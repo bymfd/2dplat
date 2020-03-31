e8fc51411f37ef67829de693840beb92df20869e
mfd <frukolosko@gmail.com>
2020-04-01 00:34:59 +0300

    artık uçuyoruz ve yavaş yavaş texturelar değişiyor

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -9,6 +9,16 @@ func _on_enemydetector_body_entered(body: PhysicsBody2D)-> void:
 	get_tree().change_scene("res://src/levels/gircikdemo.tscn")
 
 func _physics_process(delta: float) -> void :
+	if Input.is_action_just_pressed("jump"):
+		
+		get_node( "botfire" ).visible=1
+	else: 
+		pass
+	if Input.is_action_just_pressed("move_right"):
+		get_node( "player" ).set_flip_h( false )
+	elif Input.is_action_just_pressed("move_left"):
+		get_node( "player" ).set_flip_h( true )
+
 	var is_jump_interrupt := Input.is_action_just_released("jump") and _velocity.y <0.0
 	var direction : = get_direction()
 	_velocity = calculate_move_velocity(_velocity,speed,direction ,  is_jump_interrupt)
@@ -26,9 +36,13 @@ func calculate_move_velocity(
 	is_jump_interrupt: bool) -> Vector2:
 	var out := linear_velocity
 	out.x = speed.x * direction.x 
+	
 	out.y += gravity * get_physics_process_delta_time()
+	
 	if direction.y == -1.0:
+	
 		out.y= speed.y * direction.y
+		
 	if is_jump_interrupt:
 		out.y= 0.0
 	return out
